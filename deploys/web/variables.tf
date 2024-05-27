93e09f1ae63d72e721b23f6a9b6009c74a2f6e92
Matteo Valentini <matteo.valentini@nethesis.it>
2024-05-27 16:48:15 +0200

    deploys, web: cleanup unused variables

--- deploys/web/variables.tf
+++ deploys/web/variables.tf
@@ -15,22 +15,3 @@ variable "size" {
   type        = string
   default     = "s-1vcpu-1gb"
 }
-
-variable "image" {
-  description = "The image to use for the droplet"
-  type        = string
-  default     = "fedora-coreos-stable"
-}
-
-variable "ssh_keys_dir" {
-  description = "Directory containing SSH public keys"
-  type        = string
-  default     = "./ssh_keys"
-}
-
-variable "hostname" {
-  description = "The hostname to set in the Butane file"
-  type        = string
-  default     = "localhost"
-}
-
